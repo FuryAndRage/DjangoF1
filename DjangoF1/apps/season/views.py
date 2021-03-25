99ff712f2826160fa4e8375a221dd6bbd2d43dd2
Moises Andrade <lawliex@gmail.com>
2021-03-26 01:26:03 +1300

    .

--- DjangoF1/apps/season/views.py
+++ DjangoF1/apps/season/views.py
@@ -1,49 +1,50 @@
-from django.shortcuts import render
-from django.core.files import File
-from bs4 import BeautifulSoup as bs
+from django.shortcuts import render, get_object_or_404
+from DjangoF1.apps.constructor.models import Constructors
+from DjangoF1.apps.pilot.models import Pilot
 import requests
-from urllib import request as req
-from tempfile import NamedTemporaryFile
-import csv
 import os
+import  json
 
-import xml.etree.ElementTree as ET
+def get_json(season):
+    with open(f'static/driver_standing_{season}.json', 'r') as fs:
+        data = json.load(fs)
+        new_dict = []
+        standings_list = []
+        standings_dict = dict()
+        for item in data.values():
+            for i,x in item.items():
+                new_dict.append(x)
 
-import xmltodict, json
+        for item in new_dict[-1]['StandingsLists'][0]['DriverStandings']:
+            standings_dict = {
+                'driver':get_object_or_404(Pilot, driver_ref = item.get('Driver')['driverId']),
+                'constructor':get_object_or_404(Constructors, constructor_ref = item.get('Constructors')[0]['constructorId']),
+                'position':item.get('position'),
+                'points':item.get('points'),
+                'wins': item.get('wins')
+                }
+
+            standings_list.append(standings_dict)
+            
+            # print(item)
+        for item in standings_list:
+            print(item.get('driver'), item.get('position'), item.get('points'))
+        
 
 def season(request, season):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
-    req = requests.get(f'http://ergast.com/api/f1/{season}/driverStandings', headers = headers)
+    req = requests.get(f'http://ergast.com/api/f1/{season}/driverStandings.json', headers = headers)
     
-    parsed_xml = xmltodict.parse(req.content)
-    dumped_json = json.dumps(parsed_xml)
-    if os.path.isfile(f'static/driver_standing_{season}.xml'):
-        print('ja existe')
+    if os.path.isfile(f'static/driver_standing_{season}.json'):
+       get_json(season)
     else:
-        print('vou criar agora')
-        with open(f'static/driver_standing_{season}.xml', 'wb') as file:
+        with open(f'static/driver_standing_{season}.json', 'wb') as file:
             file.write(req.content)
+            get_json(season)
 
-    with open(f'static/driver_standing_{season}.xml', 'r') as fs:
-        mydoc = xmltodict.parse(fs.read())
-        json_file = json.dumps(mydoc)
-        with open(f'static/driver_standing_{season}.json', 'w') as js:
-            js.write(json_file)
-    with open(f'static/driver_standing_{season}.json', 'r') as f:
-        data = json.load(f)
-        for item in data.values():
-            for i in item.keys():
-                print(i)
-           
+       
   
 
-    # mydoc = f'static/driver_standing_{season}.xml'
-    # tree = ET.parse(mydoc)
-    # root = tree.getroot()
-    # filter = '*'
-    # for child in root.iter():
-    #     file = {child.tag:child.text}
-    #     print(file)
     
     return render(request, 'season.html')
 
